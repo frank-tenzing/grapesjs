d897a893b2363833b5c8fa31e029c606f5b809bf
Frank Wang <frank.wang@tenzing.co.nz>
2018-01-12 11:31:50 +1300

    Implementation of Saving template

--- src/block_manager/config/config.js
+++ src/block_manager/config/config.js
@@ -22,28 +22,28 @@ module.exports = {
       id: 'unEditableText', 
       label: 'Un-Editable',
       category: 'Un-Editable Blocks',
-      content: `<span contenteditable="false">If there are any mistakes or changes required to the above please 
+      content: `<div><span contenteditable="false">If there are any mistakes or changes required to the above please 
       contact us as soon as possible on</span><span> {{CALLCENTRENUMBER}} </span><span contenteditable="false">
       and quote your reference which is </span><span>{{CUSTOMERREF}}</span><span contenteditable="false">
       . You can also contact us via our website at </span><span>{{OURWEBSITE}}</span><span contenteditable="false"> 
-      particularly if you should have a change of address or contact phone numbers during the term of your contract.</span>`,
+      particularly if you should have a change of address or contact phone numbers during the term of your contract.</span></div>`,
       attributes: {class:'gjs-fonts gjs-f-text'},
     },
     // Default Blocks
     {
       id: 'sect100', 
-      category: 'Default',
+      category: 'Basic',
       label: '1 Section',
+      attributes: {class:'gjs-fonts gjs-f-b1'},
       content: `<table>
         <tr>
           <td></td>
         </tr>
         </table>`,
-      attributes: {class:'gjs-fonts gjs-f-b1'},
     },
     {
       id: 'sect50', 
-      category: 'Default',
+      category: 'Basic',
       label: '1/2 Section',
       content: `<table>
         <tr>
@@ -56,7 +56,7 @@ module.exports = {
     {
       id: 'sect30', 
       label: '1/3 Section',
-      category: 'Default',
+      category: 'Basic',
       attributes: {class:'gjs-fonts gjs-f-b3'},
       content: `<table>
         <tr>
@@ -67,16 +67,9 @@ module.exports = {
         </table>`,
     },
     {
-      id: 'button',
-      label: 'Button',
-      category: 'Default',
-      content: '<a class="button">Button</a>',
-      attributes: {class:'gjs-fonts gjs-f-button'}
-    },
-    {
       id: 'text',
       label: 'Text',
-      category: 'Default',
+      category: 'Basic',
       attributes: {class:'gjs-fonts gjs-f-text'},
       content: {
        type: 'text',
@@ -88,14 +81,14 @@ module.exports = {
     {
       id: 'text-sect',
       label: 'Text Section',
-      category: 'Default',
+      category: 'Basic',
       content: '<h1 class="heading">Insert title here</h1><p class="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>',
       attributes: {class:'gjs-fonts gjs-f-h1p'}
     },
     {
       id: 'image',
       label: 'Image',
-      category: 'Default',
+      category: 'Basic',
       attributes: {class:'gjs-fonts gjs-f-image'},
       content: {
         type:'image',
--- src/commands/view/ExportTemplate.js
+++ src/commands/view/ExportTemplate.js
@@ -1,30 +1,113 @@
 const $ = Backbone.$;
+const fileSaver = require("file-saver");
 
 module.exports = {
 
   run(editor, sender, opts = {}) {
-    sender && sender.set && sender.set('active', 0);
-    const config = editor.getConfig();
-    const modal = editor.Modal;
-    const pfx = config.stylePrefix;
-    this.cm = editor.CodeManager || null;
+
+    var pfx = editor.getConfig().stylePrefix;
+    var modal = editor.Modal;
+    var cmdm = editor.Commands;
+    var codeViewer = editor.CodeManager.getViewer('CodeMirror').clone();
+    var pnm = editor.Panels;
+    var container = document.createElement('div');
+    var btnEdit = document.createElement('button');
+
+    codeViewer.set({
+      codeName: 'htmlmixed',
+      readOnly: 0,
+      theme: 'hopscotch',
+      autoBeautify: true,
+      autoCloseTags: true,
+      autoCloseBrackets: true,
+      lineWrapping: true,
+      styleActiveLine: true,
+      smartIndent: true,
+      indentWithTabs: true
+    });
+
+    btnEdit.innerHTML = 'Edit';
+    btnEdit.className = pfx + 'btn-prim ' + pfx + 'btn-import';
+    btnEdit.onclick = function () {
+      // Get the HTML and CSS
+      var code = codeViewer.editor.getValue();
+
+      // Save the template: begin
+      try {
+        var fileName = "page.txt";
+        var blobData = new Blob([code], { type: "text/plain;charset=utf-8" });
+
+        if (typeof window.navigator.msSaveBlob !== 'undefined') {
+          window.navigator.msSaveBlob(blobData, fileName);
+        } else {
+          fileSaver.saveAs(blobData, fileName);
+        }
+      }
+      catch (err) {
+        console.log("Download failed!");
+        console.log("Exception: ", err);
+      }
+
+      modal.close();
+    };
+
+    cmdm.add('html-edit', {
+      run: function (editor, sender) {
+        sender && sender.set('active', 0);
+        var viewer = codeViewer.editor;
+        modal.setTitle('Edit code');
+        if (!viewer) {
+          var txtarea = document.createElement('textarea');
+          container.appendChild(txtarea);
+          container.appendChild(btnEdit);
+          codeViewer.init(txtarea);
+          viewer = codeViewer.editor;
+        }
+        var InnerHtml = editor.getHtml();
+        var Css = editor.getCss();
+        modal.setContent('');
+        modal.setContent(container);
+        codeViewer.setContent(InnerHtml + "<style>" + Css + '</style>');
+        modal.open();
+        viewer.refresh();
+      }
+    });
+
+    
+    pnm.addButton('options',
+      [
+        {
+          id: 'edit',
+          className: 'fa fa-edit',
+          command: 'html-edit',
+          attributes: {
+            title: 'Edit'
+          }
+        }
+      ]
+    );
+    // sender && sender.set && sender.set('active', 0);
+    // const config = editor.getConfig();
+    // const modal = editor.Modal;
+    // const pfx = config.stylePrefix;
+    // this.cm = editor.CodeManager || null;
 
 
-    if (!this.$editors) {
-      const oHtmlEd = this.buildEditor('htmlmixed', 'hopscotch', 'HTML');
-      const oCsslEd = this.buildEditor('css', 'hopscotch', 'CSS');
-      this.htmlEditor = oHtmlEd.el;
-      this.cssEditor = oCsslEd.el;
-      const $editors = $(`<div class="${pfx}export-dl"></div>`);
-      $editors.append(oHtmlEd.$el).append(oCsslEd.$el);
-      this.$editors = $editors;
-    }
+    // if (!this.$editors) {
+    //   const oHtmlEd = this.buildEditor('htmlmixed', 'hopscotch', 'HTML');
+    //   const oCsslEd = this.buildEditor('css', 'hopscotch', 'CSS');
+    //   this.htmlEditor = oHtmlEd.el;
+    //   this.cssEditor = oCsslEd.el;
+    //   const $editors = $(`<div class="${pfx}export-dl"></div>`);
+    //   $editors.append(oHtmlEd.$el).append(oCsslEd.$el);
+    //   this.$editors = $editors;
+    // }
 
-    modal.setTitle(config.textViewCode);
-    modal.setContent(this.$editors);
-    modal.open();
-    this.htmlEditor.setContent(editor.getHtml());
-    this.cssEditor.setContent(editor.getCss());
+    // modal.setTitle(config.textViewCode);
+    // modal.setContent(this.$editors);
+    // modal.open();
+    // this.htmlEditor.setContent(editor.getHtml());
+    // this.cssEditor.setContent(editor.getCss());
   },
 
   stop(editor) {
